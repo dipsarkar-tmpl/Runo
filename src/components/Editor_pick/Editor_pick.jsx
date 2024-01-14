e4b1ccca8ec0e9e34e6598774510ee1fbec2a0c3
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-14 21:45:15 +0530

    Responsive Editor Pick

--- src/components/Editor_pick/Editor_pick.jsx
+++ src/components/Editor_pick/Editor_pick.jsx
@@ -4,7 +4,7 @@ import Cards_type2 from "../Card_type2/Cards_type2"
 export default function Editor_pick() {
   return (
     <>
-    <div className="editor-container">
+    <div className="editor-container" >
         <div className="editor-title">
             <h2>Editor’s Pick</h2>
         </div>
