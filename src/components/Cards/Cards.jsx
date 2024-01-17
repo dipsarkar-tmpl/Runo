3cef8fb331830728981f2a6cf40252be73e4758c
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-17 12:01:08 +0530

    Slidable Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,68 @@
 import "./Cards.css"
 import Card from "./Card"
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { SamplePrevArrow } from "../arrows/amplePrevArrow";
+import { SampleNextArrow } from "../arrows/SampleNextArrow";
 export default function Cards() {
+  const  settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow:4 ,
+    slidesToScroll: 4,
+    rows:2,
+    initialSlide: 0,
+   
+    nextArrow: <SampleNextArrow/>,
+    prevArrow: <SamplePrevArrow/>,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+     
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          rows:1,
+          dots:false, 
+          
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows:1,
+          dots:false,
+          
+        }
+      }
+    ]
+  };
   return (
-   <>
+   
    <div className="cards-container">
+    <Slider {...settings}>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
     <Card/>
     <Card/>
     <Card/>
@@ -14,8 +72,10 @@ export default function Cards() {
     <Card/>
     <Card/>
     
+    </Slider>
+    
    </div>
    
-   </>
+   
   )
 }
