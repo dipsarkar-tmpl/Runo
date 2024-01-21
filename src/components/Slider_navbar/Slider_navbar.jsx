71f21921016c60ae9d4ea88fd2eb3b638ed5212c
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-21 19:19:17 +0530

    banner scrolling fixed

--- src/components/Slider_navbar/Slider_navbar.jsx
+++ src/components/Slider_navbar/Slider_navbar.jsx
@@ -1,55 +1,67 @@
-import Navbar from "../navbar/Navbar"
-import "./Slider_navbar.css"
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Navbar from "../navbar/Navbar";
+import "./Slider_navbar.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Slider_navbar() {
-    const  settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow:1 ,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-         
-          {
-            breakpoint: 800,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              initialSlide: 1,
-              dots:true
-            }
-          },
-          {
-            breakpoint: 680,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              dots:true
-            }
-          }
-        ]
-      };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+        },
+      },
+
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          dots: true,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+        },
+      },
+    ],
+  };
   return (
     <div className="slider">
-    <Slider {...settings}>
-      <Navbar/>
-      <Navbar/>
-      <Navbar/>
-    </Slider>
+      <Slider {...settings}>
+        <Navbar />
+        <Navbar />
+        <Navbar />
+      </Slider>
     </div>
-  )
+  );
 }
