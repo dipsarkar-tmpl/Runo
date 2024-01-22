9f8e75a5ad57199185f2c21c325bc6fdc816a139
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-22 11:48:18 +0530

    single_post integration done

--- src/components/Single_post/Single_post.jsx
+++ src/components/Single_post/Single_post.jsx
@@ -1,6 +1,36 @@
+import { useEffect } from "react";
+import axios, { Axios } from "axios";
 import "./Single_post.css";
+import { useState } from "react";
+const API = 'http://139.59.24.180:3001/api/topics/4';
+
 
 export default function Single_post() {
+  const [myData, setMyData] = useState([]);
+  console.log(myData,'myData');
+  const getApidata = async(url)=>{
+    const config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: url,
+      headers: { }
+    };
+    try {
+      const res = await  axios.request(config)
+      .then((response) => {
+        setMyData(response?.data?.data?.attributes);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+ 
+  useEffect(()=>{
+   
+    getApidata(API);
+  
+  }, []); 
+
   return (
     <>
       <div className="post-container">
