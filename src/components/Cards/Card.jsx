ce9fe2a85efc6cfc5b0080b670fed71067d33702
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-22 12:16:34 +0530

    cards changed

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,26 +1,24 @@
 import "./Card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <>
       <div className="card-container">
         
           <div className="img-container">
             <div className="cat-container">
-              <p>ADVENTURE</p>
+              <p>{props.category}</p>
             </div>
           </div>
           <div className="date-container">
-            <p>08.08.2021</p>
+            <p>{props.date}</p>
           </div>
           <div className="title-container">
-            <h2>Dream destinations to visit this year in Paris</h2>
+            <h2>{props.title}</h2>
           </div>
           <div className="des-container">
             <p>
-              Progressively incentivize cooperative systems through technically
-              sound functionalities. Credibly productivate seamless data with
-              flexible schemas.
+              {props.content}
             </p>
           </div>
         </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,31 +8,110 @@ import Card from "./Card";
 export default function Cards() {
   const [showAllCards, setShowAllCards] = useState(false);
 
+  const CardData = [
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    {
+      title: "Dream destinations to visit this year in Paris",
+      category: "Adventure",
+      date: "10-10-2001",
+      content:
+        "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas."
+    },
+    
+  ];
+
   const toggleCardDisplay = () => {
     setShowAllCards(!showAllCards);
   };
 
   return (
     <div className="cards-container">
-      {showAllCards ? (
-        <>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-        </>
-      ) : (
-        <>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-          <Link to="/single-article"><Card /></Link>
-        </>
-      )}
+      {CardData.slice(0, showAllCards ? CardData.length : 4).map((card, index) => (
+        <Link to="/single-article" key={index}>
+          <Card
+            title={card.title}
+            category={card.category}
+            date={card.date}
+            content={card.content}
+          />
+        </Link>
+      ))}
 
       <div className="btn-container">
         {showAllCards ? (
