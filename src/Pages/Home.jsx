7d2b85fed90e037d7f2d58e837064360b9255b7f
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-22 18:04:45 +0530

    test api

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Editor_pick from "../components/Editor_pick/Editor_pick";
 import Footer from "../components/Footer/Footer";
 import Most_viewed from "../components/Most_viewed_banner/Most_viewed";
@@ -8,6 +9,27 @@ import "./Home.css"
 
 
 export default function Home() {
+
+  const API =
+  'http://139.59.24.180:3001/api/topics?sort=topic_rating:desc&pagination[pageSize]=8&populate[0]=categories&populate[1]=Topic_image&populate[2]=author&populate[3]=author.author_image';
+
+  const [myData, setMyData] = useState([]);
+  console.log(myData,'myData before test commit');
+
+  const getApidata = async (url) => {
+    try {
+      const response = await axios.get(url);
+      setMyData(response?.data?.data);
+  console.log(myData,'myData after test commit');
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getApidata(API);
+  }, []);
   return (
     <>
     <Slider_navbar/>
