ceef7ae1324149430fe0ba49a804fd6919565edf
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-24 12:09:17 +0530

    card topic fixed

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,29 +1,26 @@
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-
-
 export default function Card({ category, date, content, title, image }) {
   return (
-    <Link to="/single-article" >
-    <div className="card-container">
-      <div className="img-container">
-        <img src={image} alt="Card" />
-        <div className="cat-container">
-          <p>{category}</p>
+    <Link to="/single-article">
+      <div className="card-container">
+        <div className="img-container" >
+          <img src={image} alt="Card" />
+          <div className="cat-container" >
+            <p>{category}</p>
+          </div>
+        </div>
+        <div className="date-container">
+          <p>{date}</p>
+        </div>
+        <div className="title-container">
+          <h2>{title}</h2>
+        </div>
+        <div className="des-container">
+          <p>{content}</p>
         </div>
       </div>
-      <div className="date-container">
-        <p>{date}</p>
-      </div>
-      <div className="title-container">
-        <h2>{title}</h2>
-      </div>
-      <div className="des-container">
-        <p>{content}</p>
-      </div>
-    </div>
     </Link>
-
   );
 }
