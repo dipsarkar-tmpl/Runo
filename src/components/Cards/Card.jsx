6ace06144ca97018e0137cefcc64b69fb18fc3a6
dipsarkar-tmpl <dip.s@tunica.tech>
2024-01-25 12:23:10 +0530

    minor bugs fixed

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -5,20 +5,22 @@ export default function Card({ category, date, content, title, image }) {
   return (
     <Link to="/single-article">
       <div className="card-container">
-        <div className="img-container" >
+        <div className="img-container">
           <img src={image} alt="Card" />
-          <div className="cat-container" >
+          <div className="cat-container">
             <p>{category}</p>
           </div>
         </div>
-        <div className="date-container">
-          <p>{date}</p>
-        </div>
-        <div className="title-container">
-          <h2>{title}</h2>
-        </div>
-        <div className="des-container">
-          <p>{content}</p>
+        <div className="card-content-popular">
+          <div className="date-container">
+            <p>{date}</p>
+          </div>
+          <div className="title-container">
+            <h2>{title}</h2>
+          </div>
+          <div className="des-container">
+            <p>{content}</p>
+          </div>
         </div>
       </div>
     </Link>
